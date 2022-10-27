c02eab35a289db7e7953d5e66ae50f34f253f86f
Yassir Karroum <ukarroum17@gmail.com>
2022-10-27 23:21:28 +0200

    Added some tokens scanning

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,11 +1,59 @@
+#include <sstream>
+
 #include "Scanner.h"
 
-vector<string>::Scanner scanRow(const string & row)
+vector<string>::Scanner scanTokens(const string & code)
 {
-	istringstream iss(row);
+	stringstream ss(code);
 	string token;
+	vector<Token> tokens;
 
-	while(getline(iss, token, ' '))
+	int line = 1;
+
+	for(int i = 0; i < code.length(); i++)
 	{
-		
+		switch(code[i])
+		{
+			case '(':
+				tokens.emplace_back(LEFT_PAREN, "", 0);
+				break;
+			case ')':
+				tokens.emplace_back(RIGHT_PARENT, "", 0);
+				break;
+			case '{':
+				tokens.emplace_back(LEFT_BRACE, "", 0);
+				break;
+			case '}':
+				tokens.emplace_back(RIGHT_BRACE, "", 0);
+				break;
+			case ',':
+				tokens.emplace_back(COMMA, "", 0);
+				break;
+			case '.':
+				tokens.emplace_back(DOT, "", 0);
+				break;
+			case '-':
+				tokens.emplace_back(MINUS, "", 0);
+				break;
+			case '+':
+				tokens.emplace_back(PLUS, "", 0);
+				break;
+			case ';':
+				tokens.emplace_back(SEMICOLON, "", 0);
+				break;
+			case '*':
+				tokens.emplace_back(START, "", 0);
+				break;
+			case '\n':
+				line++;	
+			case ' ':
+			case '\t':
+				break;
+			default:
+				throw std::runtime_error("Unknown token at line");
+		}
+
+		i++;
+
+	}
 }
